8a19c4031fb9c4b2a959433876bde480c610a3ad
FARHAT AGWAETEN <agwaeten@live.com>
2016-05-07 05:02:58 -0400

    Adding new output message.

--- Demo/src/Montreal.java
+++ Demo/src/Montreal.java
@@ -3,6 +3,14 @@ public class Montreal {
 
 	public static void main(String[] args) {
     System.out.println("Hello World!");
+    displayMessage();
+    
+	}
+	
+	private static void displayMessage(){
+	
+	 System.out.println("Hello Sweety!");
+		
 	}
 
 }
